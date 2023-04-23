3a1f8937a01daf2af0c8122b3fdb0ffdedbffbe7
John-Orange <91696440+John-Orange@users.noreply.github.com>
2023-04-23 22:08:03 +0000

    hw2

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 let fruits = [
-  {id:"1", name: "apple", color: "red",calories:"95",orgin:"central Asia"},
-  {id:"2", name: "banana", color: "yellow",calories:"105",orgin:"Southeast Asia"},
-  {id:"3", name: "orange", color: "orange",calories:"45",orgin:"East Asia"},
-  {id:"4", name: "blueberry", color: "blue",calories:"80",orgin:"North America"},
-  {id:"5", name: "watermelon", color: "green",calories:"85",orgin:"Africa"}
+  {id:"1", name: "apple", color: "red",calories:"95",orgin:"central Asia", taste:"sweet and sour"},
+  {id:"2", name: "banana", color: "yellow",calories:"105",orgin:"Southeast Asia", taste: "sweet"},
+  {id:"3", name: "orange", color: "orange",calories:"45",orgin:"East Asia", taste:"sweet and sour"},
+  {id:"4", name: "blueberry", color: "blue",calories:"80",orgin:"North America", taste : "sweet and sour"},
+  {id:"5", name: "watermelon", color: "green",calories:"85",orgin:"Africa", taste: "sweet"}
 ]
 
 const getAll =()=> {
