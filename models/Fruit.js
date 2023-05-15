95a20edd65e00b849d66a49a84692be024c7442b
John-Orange <91696440+John-Orange@users.noreply.github.com>
2023-05-15 23:11:30 +0000

    hw

---
+++ models/Fruit.js
@@ -0,0 +1,29 @@
+
+import mongoose from 'mongoose';
+import {connectionString} from "../credentials.js";
+const { Schema } = mongoose;
+
+// For security, connectionString should be in a separate file and excluded from git
+//const connectionString = "mongodb+srv://johnwork:zhongyuanwork@cluster0.phvv2ma.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString, {
+    dbName: 'class-projects',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('open', () => {
+  console.log('Mongoose connected.');
+});
+
+// define data model as JSON key/value pairs
+// values indicate the data type of each key
+const fruitSchema = new Schema({
+ name: { type: String, required: true },
+ color: String,
+ calories: Number,
+ orgin: String,
+ taste: String
+});
+
+export const Fruits = mongoose.model('Fruits', fruitSchema);
